759f84bd93f88e4b649522e0b1957f8bab38d11e
Dhanush123 <dhanush.patel@ymail.com>
2016-09-24 16:50:46 -0700

    fixed a lot of things
    - changed how photo taking/selecting works
    - background image now loads on lower-end devices too

--- app/src/main/java/com/x10host/dhanushpatel/findmymeal/IngredientsActivity.java
+++ app/src/main/java/com/x10host/dhanushpatel/findmymeal/IngredientsActivity.java
@@ -1,17 +1,11 @@
 package com.x10host.dhanushpatel.findmymeal;
 
-import android.Manifest;
-import android.content.DialogInterface;
 import android.content.Intent;
-import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.os.AsyncTask;
 import android.os.Bundle;
-import android.provider.MediaStore;
-import android.support.v4.app.ActivityCompat;
-import android.support.v4.content.ContextCompat;
-import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
@@ -47,10 +41,6 @@ public class IngredientsActivity extends AppCompatActivity {
     ListView listview;
     ImageView imageView;
     Button recipeFinder;
-    private boolean permsGiven = false;
-    private static final int TAKE_PICTURE = 1;
-    private static final int PICK_PHOTO = 2;
-    private static final int MY_PERMISSIONS_REQUEST_READ_EXTERNAL_STORAGE = 100;
     private Bitmap chosenBitmap;
 
     @Override
@@ -70,56 +60,43 @@ public class IngredientsActivity extends AppCompatActivity {
         ingredientsSend = new ArrayList<>();
         stringResults = new ArrayList<>();
 
-        Intent i = getIntent();
-        String actionTake = i.getStringExtra("whichImageIntent");
-        checkPermissions();
-        if (permsGiven){
-            if(actionTake.equals("photoTake")){
-                Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-                startActivityForResult(intent, TAKE_PICTURE);
+            InputStream inputStream = null;
+            try {
+                inputStream = getContentResolver().openInputStream(Uri.parse(getIntent().getStringExtra("imageUri")));
+            } catch (FileNotFoundException e) {
+                e.printStackTrace();
             }
-            else if(actionTake.equals("photoChoose")){
-                Intent intent = new Intent(Intent.ACTION_GET_CONTENT);
-                intent.setType("image/*");
-                startActivityForResult(intent, PICK_PHOTO);
-            }
-        }
-        else{
-            checkPermissions();
-        }
+            chosenBitmap = BitmapFactory.decodeStream(inputStream);
+        processStartingActivity();
 
-        clarifai = new ClarifaiClient(Constants.APP_ID,Constants.APP_SECRET);
+        clarifai = new ClarifaiClient(Constants.APP_ID, Constants.APP_SECRET);
     }
 
-    public void goToRecipeFinder (View v){
-        if(ingredientsSend.size()>=1){
-            Intent i = new Intent(IngredientsActivity.this,RecipesViewActivity.class);
-            i.putStringArrayListExtra("ingredientsUse",ingredientsSend);
+    public void goToRecipeFinder(View v) {
+        if (ingredientsSend.size() >= 1) {
+            Intent i = new Intent(IngredientsActivity.this, RecipesViewActivity.class);
+            i.putStringArrayListExtra("ingredientsUse", ingredientsSend);
             startActivity(i);
-        }
-        else{
-            Toast.makeText(IngredientsActivity.this,"No ingredients have been selected. Please choose at least 1.",Toast.LENGTH_LONG).show();
+        } else {
+            Toast.makeText(IngredientsActivity.this, "No ingredients have been selected. Please choose at least 1.", Toast.LENGTH_LONG).show();
         }
     }
 
-    private void addItemsList(){
+    private void addItemsList() {
         listview.setTextFilterEnabled(true);
-        listview.setAdapter(new ArrayAdapter<>(this,R.layout.selected_ingredients_list, stringResults));
-        listview.setOnItemClickListener(new AdapterView.OnItemClickListener(){
+        listview.setAdapter(new ArrayAdapter<>(this, R.layout.selected_ingredients_list, stringResults));
+        listview.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> adapterView, View view, int index, long id) {
                 CheckedTextView v = (CheckedTextView) view;
                 Object obj = listview.getItemAtPosition(index);
                 String item = obj.toString();
-                if(v.isChecked())
-                {
-                    if(!ingredientsSend.contains(item)){
+                if (v.isChecked()) {
+                    if (!ingredientsSend.contains(item)) {
                         ingredientsSend.add(item);
                     }
-                }
-                else
-                {
-                    if(ingredientsSend.contains(item)){
+                } else {
+                    if (ingredientsSend.contains(item)) {
                         ingredientsSend.remove(item);
                     }
                 }
@@ -128,141 +105,52 @@ public class IngredientsActivity extends AppCompatActivity {
 //        Toast.makeText(IngredientsActivity.this,"ingredients: "+ingredientsSend,Toast.LENGTH_LONG).show();
     }
 
-    private void clarifaiProcess(){
+    private void clarifaiProcess() {
 
-        if(results.get(0).getTags() != null){
+        if (results.get(0).getTags() != null) {
 
             List<Tag> tagsFound = results.get(0).getTags();
-            String tag ="";
-            for(int i=0; i < tagsFound.size();i++){
+            String tag = "";
+            for (int i = 0; i < tagsFound.size(); i++) {
                 tag = tagsFound.get(i).getName();
                 stringResults.add(tag);
                 if (i == 0) {
                     firstTag = tag;
                     tags = tag;
-                }
-                else {
+                } else {
                     tags = tags + ", " + tag;
                 }
             }
             addItemsList();
             Log.i("New photo tags are", tags);
             recipeFinder.setVisibility(View.VISIBLE);
-        }
-        else{
-            Toast.makeText(IngredientsActivity.this,"No food items could be found. Please try again.",Toast.LENGTH_LONG).show();
-            Log.i("No tags","could be found...");
+        } else {
+            Toast.makeText(IngredientsActivity.this, "No food items could be found. Please try again.", Toast.LENGTH_LONG).show();
+            Log.i("No tags", "could be found...");
         }
     }
 
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent intent) {
-        if ((requestCode ==  TAKE_PICTURE || requestCode ==  PICK_PHOTO) && resultCode == RESULT_OK && intent != null) {
+    private void processStartingActivity(){
+        imageView.setImageBitmap(chosenBitmap);
 
-            InputStream inputStream = null;
-            try {
-                inputStream = getContentResolver().openInputStream(intent.getData());
-            } catch (FileNotFoundException e) {
-                e.printStackTrace();
+        ByteArrayOutputStream stream = new ByteArrayOutputStream();
+        chosenBitmap.compress(Bitmap.CompressFormat.JPEG, 100, stream);
+        photoBytes = stream.toByteArray();
+
+        new AsyncTask<Void, Void, Void>() {
+            @Override
+            protected Void doInBackground(final Void... params) {
+                // something you know that will take a few seconds
+                results = clarifai.recognize(new RecognitionRequest(photoBytes).setModel("food-items-v0.1"));
+                return null;
             }
-            chosenBitmap = BitmapFactory.decodeStream(inputStream);
 
-            imageView.setImageBitmap(chosenBitmap);
-
-            ByteArrayOutputStream stream = new ByteArrayOutputStream();
-            chosenBitmap.compress(Bitmap.CompressFormat.JPEG, 100, stream);
-            photoBytes = stream.toByteArray();
-
-            new AsyncTask<Void, Void, Void>() {
-                @Override
-                protected Void doInBackground(final Void... params) {
-                    // something you know that will take a few seconds
-                    results = clarifai.recognize(new RecognitionRequest(photoBytes).setModel("food-items-v0.1"));
-                    return null;
-                }
-
-                @Override
-                protected void onPostExecute(final Void result) {
-                    // continue what you are doing...
-                    clarifaiProcess();
-                    Log.i("Clarifai","recognition done.");
-                }
-            }.executeOnExecutor(AsyncTask.THREAD_POOL_EXECUTOR, null);
-        }
-    }
-
-    private void checkPermissions() {
-        if (ContextCompat.checkSelfPermission(IngredientsActivity.this,
-                Manifest.permission.READ_EXTERNAL_STORAGE)
-                != PackageManager.PERMISSION_GRANTED) {
-
-            permsGiven=false;
-            // Should we show an explanation?
-            if (ActivityCompat.shouldShowRequestPermissionRationale(IngredientsActivity.this,
-                    Manifest.permission.READ_EXTERNAL_STORAGE)) {
-
-                permissionFailDialogCreate();
-
-                // Show an expanation to the user *asynchronously* -- don't block
-                // this thread waiting for the user's response! After the user
-                // sees the explanation, try again to request the permission.
-
-            } else {
-
-                // No explanation needed, we can request the permission.
-
-                ActivityCompat.requestPermissions(IngredientsActivity.this,
-                        new String[]{Manifest.permission.READ_EXTERNAL_STORAGE},
-                        MY_PERMISSIONS_REQUEST_READ_EXTERNAL_STORAGE);
-
-                // MY_PERMISSIONS_REQUEST_READ_CONTACTS is an
-                // app-defined int constant. The callback method gets the
-                // result of the request.
+            @Override
+            protected void onPostExecute(final Void result) {
+                // continue what you are doing...
+                clarifaiProcess();
+                Log.i("Clarifai","recognition done.");
             }
-        }
-        else{
-            permsGiven=true;
-        }
-    }
-
-    private void permissionFailDialogCreate(){
-        AlertDialog.Builder builder = new AlertDialog.Builder(this);
-        builder.setMessage("You need to allow the permission(s) for the app to interact with photos and videos on your device.")
-                .setTitle("Unable to access files")
-                .setCancelable(false)
-                .setPositiveButton("Enable",
-                        new DialogInterface.OnClickListener() {
-                            public void onClick(DialogInterface dialog, int id) {
-                                dialog.cancel();
-                                ActivityCompat.requestPermissions(IngredientsActivity.this,
-                                        new String[]{Manifest.permission.READ_EXTERNAL_STORAGE},
-                                        MY_PERMISSIONS_REQUEST_READ_EXTERNAL_STORAGE);
-                            }
-                        }
-                )
-                .setNegativeButton("Cancel",
-                        new DialogInterface.OnClickListener() {
-                            public void onClick(DialogInterface dialog, int id) {
-                                dialog.cancel();
-                            }
-                        }
-                );
-        AlertDialog alert = builder.create();
-        alert.show();
-    }
-
-    @Override
-    public void onRequestPermissionsResult(int requestCode, String permissions[], int[] grantResults) {
-        switch (requestCode) {
-            case MY_PERMISSIONS_REQUEST_READ_EXTERNAL_STORAGE:{
-                if(grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED){
-                    // permissions granted.
-                    permsGiven=true;
-                } else {
-                    permissionFailDialogCreate();
-                }
-                return;
-            }
-        }
+        }.executeOnExecutor(AsyncTask.THREAD_POOL_EXECUTOR, null);
     }
 }
--- app/src/main/java/com/x10host/dhanushpatel/findmymeal/StartingActivity.java
+++ app/src/main/java/com/x10host/dhanushpatel/findmymeal/StartingActivity.java
@@ -1,21 +1,30 @@
 package com.x10host.dhanushpatel.findmymeal;
 
+import android.Manifest;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.view.WindowManager;
 import android.widget.ImageButton;
-import android.widget.ImageView;
-import android.widget.RelativeLayout;
-
-import com.squareup.picasso.Callback;
-import com.squareup.picasso.Picasso;
 
 public class StartingActivity extends AppCompatActivity {
 
     ImageButton galleryButton, cameraButton, settingsButton;
+    private Bitmap chosenBitmap;
     Intent i;
+
+    private static final int TAKE_PICTURE = 1;
+    private static final int PICK_PHOTO = 2;
+    private boolean permsGiven = false;
+    private static final int MY_PERMISSIONS_REQUEST_READ_EXTERNAL_STORAGE = 100;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,27 +33,7 @@ public class StartingActivity extends AppCompatActivity {
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
                 WindowManager.LayoutParams.FLAG_FULLSCREEN);
 
-        final RelativeLayout layout = (RelativeLayout) findViewById(R.id.startRelLay);
-
         setContentView(R.layout.activity_starting);
-
-        final ImageView img = new ImageView(this);
-        Picasso.with(this)
-                .load(R.drawable.cottoncandy)
-                .fit()
-                .centerCrop()
-                .into(img, new Callback() {
-                    @Override
-                    public void onSuccess() {
-
-                        layout.setBackgroundDrawable(img.getDrawable());
-                    }
-
-                    @Override
-                    public void onError() {
-
-                    }
-                });
 
         galleryButton = (ImageButton) findViewById(R.id.galleryButton);
         galleryButton.setImageResource(R.drawable.galleryimg);
@@ -55,16 +44,133 @@ public class StartingActivity extends AppCompatActivity {
     }
 
     public void photoTake (View v){
-        i = new Intent(StartingActivity.this,IngredientsActivity.class);
-        i.putExtra("whichImageIntent","photoTake");
-        startActivity(i);
+        if(permsGiven) {
+            i = new Intent(StartingActivity.this, IngredientsActivity.class);
+            Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+            startActivityForResult(intent, TAKE_PICTURE);
+        }
+        else{
+            checkPermissions();
+        }
+//        i.putExtra("whichImageIntent","photoTake");
+//        startActivity(i);
     }
 
     public void photoChoose (View v){
-        i = new Intent(StartingActivity.this,IngredientsActivity.class);
-        i.putExtra("whichImageIntent","photoChoose");
-        startActivity(i);
+        if(permsGiven) {
+            i = new Intent(StartingActivity.this, IngredientsActivity.class);
+            Intent intent = new Intent(Intent.ACTION_GET_CONTENT);
+            intent.setType("image/*");
+            startActivityForResult(intent, PICK_PHOTO);
+        }
+        else{
+            checkPermissions();
+        }
+//        i.putExtra("whichImageIntent","photoChoose");
+//        startActivity(i);
     }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent intent) {
+        if ((requestCode ==  TAKE_PICTURE || requestCode ==  PICK_PHOTO) && resultCode == RESULT_OK && intent != null) {
+
+//            InputStream inputStream = null;
+//            try {
+//                inputStream = getContentResolver().openInputStream(intent.getData());
+//            } catch (FileNotFoundException e) {
+//                e.printStackTrace();
+//            }
+//            chosenBitmap = BitmapFactory.decodeStream(inputStream);
+//
+//            //Convert to byte array
+//            ByteArrayOutputStream stream = new ByteArrayOutputStream();
+//            chosenBitmap.compress(Bitmap.CompressFormat.PNG, 50, stream);
+//            byte[] byteArray = stream.toByteArray();
+
+            Intent i2 = new Intent(StartingActivity.this,IngredientsActivity.class);
+            i2.putExtra("imageUri", intent.getData().toString());
+            startActivity(i2);
+
+            //------------
+
+        }
+    }
+
+    private void checkPermissions() {
+        if (ContextCompat.checkSelfPermission(StartingActivity.this,
+                Manifest.permission.READ_EXTERNAL_STORAGE)
+                != PackageManager.PERMISSION_GRANTED) {
+
+            permsGiven=false;
+            // Should we show an explanation?
+            if (ActivityCompat.shouldShowRequestPermissionRationale(StartingActivity.this,
+                    Manifest.permission.READ_EXTERNAL_STORAGE)) {
+
+                permissionFailDialogCreate();
+
+                // Show an expanation to the user *asynchronously* -- don't block
+                // this thread waiting for the user's response! After the user
+                // sees the explanation, try again to request the permission.
+
+            } else {
+
+                // No explanation needed, we can request the permission.
+
+                ActivityCompat.requestPermissions(StartingActivity.this,
+                        new String[]{Manifest.permission.READ_EXTERNAL_STORAGE},
+                        MY_PERMISSIONS_REQUEST_READ_EXTERNAL_STORAGE);
+
+                // MY_PERMISSIONS_REQUEST_READ_CONTACTS is an
+                // app-defined int constant. The callback method gets the
+                // result of the request.
+            }
+        }
+        else{
+            permsGiven=true;
+        }
+    }
+
+    private void permissionFailDialogCreate(){
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setMessage("You need to allow the permission(s) for the app to interact with photos and videos on your device.")
+                .setTitle("Unable to access files")
+                .setCancelable(false)
+                .setPositiveButton("Enable",
+                        new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog, int id) {
+                                dialog.cancel();
+                                ActivityCompat.requestPermissions(StartingActivity.this,
+                                        new String[]{Manifest.permission.READ_EXTERNAL_STORAGE},
+                                        MY_PERMISSIONS_REQUEST_READ_EXTERNAL_STORAGE);
+                            }
+                        }
+                )
+                .setNegativeButton("Cancel",
+                        new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog, int id) {
+                                dialog.cancel();
+                            }
+                        }
+                );
+        AlertDialog alert = builder.create();
+        alert.show();
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String permissions[], int[] grantResults) {
+        switch (requestCode) {
+            case MY_PERMISSIONS_REQUEST_READ_EXTERNAL_STORAGE:{
+                if(grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED){
+                    // permissions granted.
+                    permsGiven=true;
+                } else {
+                    permissionFailDialogCreate();
+                }
+                return;
+            }
+        }
+    }
+
 
     public void goToSettings (View v){
         i = new Intent(StartingActivity.this,SettingsActivity.class);
